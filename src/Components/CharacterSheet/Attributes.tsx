377c8643da0188bac11a9fd242ad34f62e7e8df5
Janne Lavila <janne.lavila@wheelq.com>
2019-11-23 20:31:34 +0200

    finish css

--- src/Components/CharacterSheet/Attributes.tsx
+++ src/Components/CharacterSheet/Attributes.tsx
@@ -45,31 +45,39 @@ function calculateModifier(value: number) {
 const useAttributeStyles = createUseStyles<string>((theme: Theme) => ({
   root: {
     display: 'grid',
-    gridTemplateColumns: '40px 40px 40px auto'
+    gridTemplateColumns: '40px 40px 40px auto',
+    gridColumnGap: 10
   },
   title: {
     gridColumnStart: 2,
     justifySelf: 'center'
   },
-  score: {
+  scoreRoot: {
     border: theme.border,
     gridRowStart: 2,
     gridColumnStart: 2,
     justifySelf: 'center',
     width: 40,
     height: 40,
-    textAlign: 'center'
+    display: 'flex',
+    justifyContent: 'center'
   },
-  modifier: {
+  score: {
+    alignSelf: 'center'
+  },
+  modifierRoot: {
     gridRowStart: 2,
     gridColumnStart: 3,
     border: theme.border,
     borderRadius: '100%',
-    textAlign: 'center',
-    margin: 5,
-    justifySelf: 'center',
-    width: 30,
-    height: 30
+    margin: 1,
+    display: 'flex',
+    justifyContent: 'center',
+    width: 40,
+    height: 40
+  },
+  modifier: {
+    alignSelf: 'center'
   },
   details: {
     gridRowStart: 2,
@@ -88,8 +96,12 @@ function Attribute({ title, score }: AttributeProps) {
   return (
     <div className={classes.root}>
       <label className={classes.title}>{title}</label>
-      <span className={classes.score}>{score}</span>
-      <span className={classes.modifier}>{calculateModifier(score)}</span>
+      <div className={classes.scoreRoot}>
+        <span className={classes.score}>{score}</span>
+      </div>
+      <div className={classes.modifierRoot}>
+        <span className={classes.modifier}>{calculateModifier(score)}</span>
+      </div>
       <span className={classes.details}>{ATTRIBUTE_DETAILS[title]}</span>
     </div>
   )
