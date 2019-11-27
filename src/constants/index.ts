ae66b808e5f13278f065401af6b59bc9a9b36836
Janne Lavila <janne.lavila@wheelq.com>
2019-11-27 18:56:46 +0200

    add constant

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -7,6 +7,15 @@ const ATTRIBUTE_DETAILS: { [name: string]: string } = {
   wisdom: "Saves\xa0vs\xa0Non-Magic\xa0Effects"
 }
 
+const SAVING_THROW_DETAILS = {
+  paralyze: "Mobility Hazards (Petrification, Hold, Etc.",
+  poison: "Instant Death/KO Situations",
+  breathWeapon: "Area Effects",
+  magicalDevice: "Spell-Like Effects from Items",
+  magic: "Spells or Innate Abilities"
+}
+
 export {
-  ATTRIBUTE_DETAILS
+  ATTRIBUTE_DETAILS,
+  SAVING_THROW_DETAILS
 }
