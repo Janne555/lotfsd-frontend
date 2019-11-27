e89367102854f41ed02009ee5bd5c32e80d8bee0
Janne Lavila <janne.lavila@wheelq.com>
2019-11-27 15:51:26 +0200

    reacts to changes

--- src/Components/CharacterSheet/Attribute.tsx
+++ src/Components/CharacterSheet/Attribute.tsx
@@ -1,26 +1,32 @@
 
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { createUseStyles } from 'react-jss'
 import { ATTRIBUTE_DETAILS } from '../../constants'
 import { calculateModifier } from '../../services'
+import { useDispatch } from '../../hooks'
+import { updateAttribute } from '../../Redux/actions'
 
 type AttributeProps = {
-  title: string
+  title: keyof Attributes
   score: number
   index: number
-  onChange: (name: string, value: number) => void
+}
+
+type StyleProps = {
+  index: number,
+  error: boolean
 }
 
 const useAttributeStyles = createUseStyles((theme: Theme) => ({
   title: {
     gridColumnStart: 2,
-    gridRowStart: (index: number) => (index + 1) * 2 - 1,
+    gridRowStart: ({ index }: StyleProps) => (index + 1) * 2 - 1,
     justifySelf: 'center',
     alignSelf: 'end'
   },
   scoreRoot: {
     border: theme.border,
-    gridRowStart: (index: number) => (index + 1) * 2,
+    gridRowStart: ({ index }: StyleProps) => (index + 1) * 2,
     gridColumnStart: 2,
     justifySelf: 'center',
     width: 40,
@@ -31,10 +37,11 @@ const useAttributeStyles = createUseStyles((theme: Theme) => ({
   score: {
     width: '100%',
     textAlign: 'center',
-    fontSize: '1em'
+    fontSize: '1em',
+    color: ({ error }: StyleProps) => error ? 'red' : ''
   },
   modifierRoot: {
-    gridRowStart: (index: number) => (index + 1) * 2,
+    gridRowStart: ({ index }: StyleProps) => (index + 1) * 2,
     gridColumnStart: 3,
     border: theme.border,
     borderRadius: '100%',
@@ -48,24 +55,38 @@ const useAttributeStyles = createUseStyles((theme: Theme) => ({
     alignSelf: 'center'
   },
   details: {
-    gridRowStart: (index: number) => (index + 1) * 2,
+    gridRowStart: ({ index }: StyleProps) => (index + 1) * 2,
     gridColumnStart: 4
   }
 }))
 
 
-function Attribute({ title, score, index, onChange }: AttributeProps) {
-  const classes = useAttributeStyles(index)
+function Attribute({ title, score, index }: AttributeProps) {
+  const inputRef = useRef<HTMLInputElement>(null)
+  const [error, setError] = useState(false)
+  const classes = useAttributeStyles({ index, error })
+  const dispatch = useDispatch()
 
-  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
-    onChange(title, Number(e.target.value))
+  useEffect(() => {
+    if (inputRef.current)
+      inputRef.current.value = `${score}`
+  }, [score])
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>): void {
+    const value = parseInt(e.target.value)
+    if (!isNaN(value)) {
+      dispatch(updateAttribute(title, value))
+      setError(false)
+    } else {
+      setError(true)
+    }
   }
 
   return (
     <>
       <label className={classes.title}>{title}</label>
       <div className={classes.scoreRoot}>
-        <input className={classes.score} value={score} onChange={handleChange}></input>
+        <input ref={inputRef} type="number" className={classes.score} defaultValue={score} onChange={handleChange}></input>
       </div>
       <div className={classes.modifierRoot}>
         <span className={classes.modifier}>{calculateModifier(score)}</span>
