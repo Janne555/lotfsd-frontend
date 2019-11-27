7473b2a1fc87e0c7dab7cc96429f7a8c72cca7a8
Janne Lavila <janne.lavila@wheelq.com>
2019-11-27 12:18:03 +0200

    add constants

---
+++ src/constants/index.ts
@@ -0,0 +1,12 @@
+const ATTRIBUTE_DETAILS: { [name: string]: string } = {
+  charisma: "Retainer Recruitment, Loyalty",
+  constitution: "Hit Points, Daily Travel Distance",
+  dexterity: "AC, Ranged AB, Initiative",
+  intelligence: "Saves vs Magic Effects",
+  strength: "Mêlée AB, Open Doors",
+  wisdom: "Saves vs Non-Magic Effects"
+}
+
+export {
+  ATTRIBUTE_DETAILS
+}
