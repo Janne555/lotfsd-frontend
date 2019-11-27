ac4a97c2598afc175c1241bdf3382aea26de7446
Janne Lavila <janne.lavila@wheelq.com>
2019-11-27 18:56:59 +0200

    combine reducer

--- src/Redux/reducers/characterSheet.ts
+++ src/Redux/reducers/characterSheet.ts
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
 import attributes from './attributes'
+import savingThrows from './savingThrows'
 
 export default combineReducers({
-  attributes
+  attributes,
+  savingThrows
 })
