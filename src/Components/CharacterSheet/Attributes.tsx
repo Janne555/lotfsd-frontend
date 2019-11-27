eacf237c67142b0ffcf9b62cdd85bb73c39db0ec
Janne Lavila <janne.lavila@wheelq.com>
2019-11-27 13:09:07 +0200

    improve styles

--- src/Components/CharacterSheet/Attributes.tsx
+++ src/Components/CharacterSheet/Attributes.tsx
@@ -7,39 +7,61 @@ import { calculateModifier } from '../../services'
 
 const useStyles = createUseStyles((theme: Theme) => ({
   root: {
-    display: 'flex',
-    flexDirection: 'column'
+    display: 'grid',
+    gridTemplateColumns: '40px 40px 40px auto',
+    gridTemplateRows: '20px auto',
+    gridColumnGap: 10,
+    gridRowGap: 10,
+    border: theme.border,
+    padding: theme.padding
+  },
+  leftTitle: {
+    gridRowStart: 2,
+    gridRowEnd: (numItems: number) => numItems * 2 + 1,
+    fontSize: '1.5em',
+    writingMode: 'vertical-rl',
+    transform: 'rotate(180deg)',
+    alignSelf: 'center'
+  },
+  topTitle: {
+    gridColumnStart: 4,
+    fontSize: '1.5em'
   }
 }))
 
 export default function Attributes() {
   const attributes = useSelector<Attributes>(selectAttributes)
-  const classes = useStyles()
+  const classes = useStyles(Object.keys(attributes).length)
 
   return (
     <div className={classes.root}>
+      <span className={classes.leftTitle}>Ability Scores</span>
+      <span className={classes.topTitle}>Modifiers</span>
       {
-        Object.entries(attributes).map(([name, value]) => (
-          <Attribute key={name} title={name} score={value} />
+        Object.entries(attributes).map(([name, value], index) => (
+          <Attribute key={name} title={name} score={value} index={index} />
         ))
       }
     </div>
   )
 }
 
+
+type AttributeProps = {
+  title: string
+  score: number
+  index: number
+}
+
 const useAttributeStyles = createUseStyles((theme: Theme) => ({
-  root: {
-    display: 'grid',
-    gridTemplateColumns: '40px 40px 40px auto',
-    gridColumnGap: 10
-  },
   title: {
     gridColumnStart: 2,
+    gridRowStart: (index: number) => (index + 1) * 2 - 1,
     justifySelf: 'center'
   },
   scoreRoot: {
     border: theme.border,
-    gridRowStart: 2,
+    gridRowStart: (index: number) => (index + 1) * 2,
     gridColumnStart: 2,
     justifySelf: 'center',
     width: 40,
@@ -51,7 +73,7 @@ const useAttributeStyles = createUseStyles((theme: Theme) => ({
     alignSelf: 'center'
   },
   modifierRoot: {
-    gridRowStart: 2,
+    gridRowStart: (index: number) => (index + 1) * 2,
     gridColumnStart: 3,
     border: theme.border,
     borderRadius: '100%',
@@ -65,21 +87,17 @@ const useAttributeStyles = createUseStyles((theme: Theme) => ({
     alignSelf: 'center'
   },
   details: {
-    gridRowStart: 2,
+    gridRowStart: (index: number) => (index + 1) * 2,
     gridColumnStart: 4
   }
 }))
 
-type AttributeProps = {
-  title: string
-  score: number
-}
 
-function Attribute({ title, score }: AttributeProps) {
-  const classes = useAttributeStyles()
+function Attribute({ title, score, index }: AttributeProps) {
+  const classes = useAttributeStyles(index)
 
   return (
-    <div className={classes.root}>
+    <>
       <label className={classes.title}>{title}</label>
       <div className={classes.scoreRoot}>
         <span className={classes.score}>{score}</span>
@@ -88,6 +106,6 @@ function Attribute({ title, score }: AttributeProps) {
         <span className={classes.modifier}>{calculateModifier(score)}</span>
       </div>
       <span className={classes.details}>{ATTRIBUTE_DETAILS[title]}</span>
-    </div>
+    </>
   )
 }
