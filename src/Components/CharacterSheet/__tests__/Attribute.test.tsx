25a71e6ff4edd5dca20f57e64706ae0569b1e2a2
Janne Lavila <janne.lavila@wheelq.com>
2019-11-27 16:57:42 +0200

    add some tests

---
+++ src/Components/CharacterSheet/__tests__/Attribute.test.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { render, getByLabelText } from '@testing-library/react'
+import Attribute from '../Attribute'
+import * as hooks from '../../../hooks/redux'
+import userEvent from '@testing-library/user-event'
+
+describe('<Attribute />', () => {
+  const mockDispatch = jest.fn()
+  const mockUseDispatch = jest.spyOn(hooks, 'useDispatch').mockImplementation((): any => mockDispatch)
+
+  afterEach(mockUseDispatch.mockClear)
+
+  it('should display a value', () => {
+    const { queryByLabelText } = render(<Attribute index={1} score={15} title="charisma" />)
+    expect(queryByLabelText("charisma")).toHaveAttribute("value", "15")
+  });
+
+  it('should send action when changing value', () => {
+    const { getByLabelText } = render(<Attribute index={1} score={15} title="charisma" />)
+    userEvent.type(getByLabelText("charisma"), "20")
+    expect(mockDispatch).toHaveBeenCalledTimes(2)
+  });
+})
