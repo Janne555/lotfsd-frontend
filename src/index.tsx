8f6fa09310eda90a34313690d0722c9348c7d74b
Janne Lavila <janne.lavila@wheelq.com>
2019-11-27 14:16:32 +0200

    remove semicolons

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
 import store from './Redux/store'
 import { Provider } from 'react-redux'
 import { ThemeProvider } from 'react-jss'
-import { theme } from './styles/theme';
+import { theme } from './styles/theme'
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
