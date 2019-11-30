a317aa7eb8cce1e5a03f85aab7dc9d5cb7de45a5
Janne Lavila <janne.lavila@wheelq.com>
2019-11-30 14:09:20 +0200

    add tests

---
+++ src/Components/CharacterSheet/__tests__/SavingThrows.test.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { SAVING_THROW_TITLES } from '../../../constants'
+import { renderWrapped } from '../../../setupTests'
+import SavingThrows from '../SavingThrows'
+import { savingThrows } from '../../../testData/initialState'
+
+describe('<SavingThrows />', () => {
+  it.each(Object.values(SAVING_THROW_TITLES).map(title => title.replace(/\s/g, '')))(
+    'expect title "%s" to be in the document',
+    (title) => {
+      const { queryByText } = renderWrapped(<SavingThrows />)
+      expect(queryByText(text => text.replace(/\s/g, '') === title)).toBeInTheDocument()
+    }
+  );
+
+  it.each(Object.values(savingThrows).map(number => `${number}`))(
+    'expect value "%s" to be in the document',
+    (value) => {
+      const { queryByText } = renderWrapped(<SavingThrows />)
+      expect(queryByText(value)).toBeInTheDocument()
+    }
+  );
+})
