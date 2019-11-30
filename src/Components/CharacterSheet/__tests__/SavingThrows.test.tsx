c2ee78274b0e99cec4e08c0f9b07530658e2d081
Janne Lavila <janne.lavila@wheelq.com>
2019-11-30 15:35:37 +0200

    remove unused import

--- src/Components/CharacterSheet/__tests__/SavingThrows.test.tsx
+++ src/Components/CharacterSheet/__tests__/SavingThrows.test.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { SAVING_THROW_TITLES } from '../../../constants'
 import { renderWrapped } from '../../../setupTests'
 import SavingThrows from '../SavingThrows'
-import { savingThrows } from '../../../testData/initialState'
 
 describe('<SavingThrows />', () => {
   it.each(Object.values(SAVING_THROW_TITLES).map(title => title.replace(/\s/g, '')))(
