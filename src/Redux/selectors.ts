b6937ebbef5d9d0d92e425ff62ab911dfdf45e41
Janne Lavila <janne.lavila@wheelq.com>
2019-11-30 15:33:42 +0200

    add selectors

--- src/Redux/selectors.ts
+++ src/Redux/selectors.ts
@@ -1,10 +1,25 @@
-import { calculateSavingThrows } from "../services"
+import { calculateSavingThrows, calculateMeleeAttackBonus } from "../services"
 
 const selectAttributes = (state: RootState): Attributes => state.characterSheet.attributes
 
 const selectSavingThrows = (state: RootState): SavingThrows => calculateSavingThrows(state)
 
+const selectMaxHP = (state: RootState): number => state.characterSheet.maxHP
+
+const selecCurrentHP = (state: RootState): number => state.characterSheet.currentHP
+
+const selectBaseAttackBonus = (state: RootState): number => state.characterSheet.attackBonus
+
+const selectMeleeAttackBonus = (state: RootState): number => calculateMeleeAttackBonus(selectBaseAttackBonus(state), selectAttributes(state).strength)
+
+const selectRangedAttackBonus = (state: RootState): number => calculateMeleeAttackBonus(selectBaseAttackBonus(state), selectAttributes(state).dexterity)
+
 export {
   selectAttributes,
-  selectSavingThrows
+  selectSavingThrows,
+  selecCurrentHP,
+  selectMaxHP,
+  selectMeleeAttackBonus,
+  selectRangedAttackBonus,
+  selectBaseAttackBonus
 }
