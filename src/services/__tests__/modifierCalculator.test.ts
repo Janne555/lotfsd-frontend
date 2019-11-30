5674eaf32e0a9d5e301d70126348bd1eb37dbe75
Janne Lavila <janne.lavila@wheelq.com>
2019-11-30 15:42:58 +0200

    implement test

--- src/services/__tests__/modifierCalculator.test.ts
+++ src/services/__tests__/modifierCalculator.test.ts
@@ -1,5 +1,9 @@
-import { calculateModifier } from '../index'
+import { calculateModifier } from '../index'
 
 describe('test suite', () => {
-  it.todo('should calculate correct values')
+  it.each([[3, -3], [5, -2], [7, -1], [8, -1], [9, 0], [15, 1], [16, 2], [18, 3]])(
+    "for value %d the modifier should be %d",
+    (input, output) => {
+      expect(calculateModifier(input)).toBe(output)
+    })
 })
