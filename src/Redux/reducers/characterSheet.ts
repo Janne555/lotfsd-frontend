da18262d9f01e9743cc92320a5eafacbd9a10ac7
Janne Lavila <janne.lavila@wheelq.com>
2019-11-30 15:33:37 +0200

    add reducers

--- src/Redux/reducers/characterSheet.ts
+++ src/Redux/reducers/characterSheet.ts
@@ -1,8 +1,17 @@
 import { combineReducers } from 'redux'
 import attributes from './attributes'
 import savingThrows from './savingThrows'
+import { createReducer } from 'typesafe-actions'
+import { initialState } from '../../testData/initialState'
+
+const attackBonus = createReducer(0)
+const maxHP = createReducer(0)
+const currentHP = createReducer(0)
 
 export default combineReducers({
   attributes,
-  savingThrows
+  savingThrows,
+  attackBonus,
+  maxHP,
+  currentHP
 })
