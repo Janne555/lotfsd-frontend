359d45d506486a7b6a66ec0386d0d1964a8a0eb2
Janne Lavila <janne.lavila@wheelq.com>
2019-11-30 14:09:45 +0200

    add initial state

---
+++ src/testData/initialState.ts
@@ -0,0 +1,29 @@
+const attributes = {
+  charisma: 18,
+  constitution: 17,
+  dexterity: 16,
+  intelligence: 15,
+  strength: 14,
+  wisdom: 13
+}
+
+const savingThrows = {
+  breathWeapon: 10,
+  magic: 11,
+  magicalDevice: 12,
+  paralyze: 13,
+  poison: 14
+}
+
+const initialState: RootState = {
+  characterSheet: {
+    attributes,
+    savingThrows
+  }
+}
+
+export {
+  attributes,
+  savingThrows,
+  initialState
+}
