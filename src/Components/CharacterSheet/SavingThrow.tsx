59a99c90581a045f37ebb1ebb0ca06384c195523
Janne Lavila <janne.lavila@wheelq.com>
2019-11-30 14:39:48 +0200

    extract component

--- src/Components/CharacterSheet/SavingThrow.tsx
+++ src/Components/CharacterSheet/SavingThrow.tsx
@@ -1,50 +1,31 @@
 import React from 'react'
 import { createUseStyles } from 'react-jss'
 import { SAVING_THROW_DETAILS, SAVING_THROW_TITLES } from '../../constants'
+import RotatedCube from '../_shared/RotatedCube'
 
 type Props = {
   title: keyof SavingThrows,
-  value: number,
-  index: number
+  value: number
 }
 
-const useStyles = createUseStyles((theme: Theme) => ({
+const useStyles = createUseStyles({
   savingThrowRoot: {
     display: 'grid',
     gridTemplateRows: '20px 40px auto',
     gridRowGap: 15,
     textAlign: 'center',
     width: 100
-  },
-  title: {
-  },
-  value: {
-    width: 40,
-    height: 40,
-    display: 'flex',
-    transform: 'rotate(45deg)',
-    border: theme.border,
-    justifyContent: 'space-around',
-    justifySelf: 'center',
-    '& span': {
-      alignSelf: 'center',
-      transform: 'rotate(-45deg)'
-    }
-  },
-  details: {
   }
-}))
+})
 
-export default function SavingThrow({ title, value, index }: Props) {
-  const classes = useStyles(index)
+export default function SavingThrow({ title, value }: Props) {
+  const classes = useStyles()
 
   return (
     <div className={classes.savingThrowRoot}>
-      <h3 className={classes.title}>{SAVING_THROW_TITLES[title]}</h3>
-      <div className={classes.value}>
-        <span>{value}</span>
-      </div>
-      <span className={classes.details}>{SAVING_THROW_DETAILS[title]}</span>
+      <h3>{SAVING_THROW_TITLES[title]}</h3>
+      <RotatedCube value={value} />
+      <span>{SAVING_THROW_DETAILS[title]}</span>
     </div>
   )
 }
