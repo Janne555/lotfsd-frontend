7f4fc0ecf888a6500987a4c0e7ea20c8bc108f06
Janne Lavila <janne.lavila@wheelq.com>
2019-12-01 12:00:43 +0200

    add component

---
+++ src/Components/_shared/Cube.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { createUseStyles } from 'react-jss'
+
+type Props = {
+  children: string | number
+}
+
+const useStyles = createUseStyles((theme: Theme) => ({
+  rotatedCube: {
+    width: 40,
+    height: 40,
+    display: 'flex',
+    border: theme.border,
+    justifyContent: 'space-around',
+    justifySelf: 'center',
+    '& span': {
+      alignSelf: 'center',
+    }
+  }
+}))
+
+function Cube({ children }: Props) {
+  const classes = useStyles()
+
+  return (
+    <div className={classes.rotatedCube}>
+      <span>{children}</span>
+    </div>
+  )
+}
+
+export default Cube
