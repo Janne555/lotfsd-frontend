93e38816c6bd624bc89372ff9fa57ed2758e57a8
Janne Lavila <janne.lavila@wheelq.com>
2019-12-01 14:15:21 +0200

    add triangle component

---
+++ src/Components/_shared/Triangle.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { createUseStyles } from 'react-jss'
+
+const useStyles = createUseStyles((theme: Theme) => ({
+  triangleRoot: {
+    display: 'flex',
+    position: 'relative',
+    backgroundColor: 'black',
+    height: 54,
+    width: 54,
+    clipPath: 'polygon(50% 100%, 0 0%, 100% 0%)',
+    '&:after': {
+      position: 'absolute',
+      top: 2,
+      left: 3,
+      content: '""',
+      backgroundColor: 'white',
+      height: 48,
+      width: 48,
+      clipPath: 'polygon(50% 100%, 0 0%, 100% 0%)'
+    },
+    '& span': {
+      margin: '20% auto',
+      // top: '50%',
+      zIndex: 1
+    }
+  },
+}))
+
+type Props = {
+  children: string | number
+}
+
+function Triangle({ children }: Props) {
+  const classes = useStyles()
+  return (
+    <div className={classes.triangleRoot}>
+      <span>{children}</span>
+    </div>
+  )
+}
+
+export default Triangle
