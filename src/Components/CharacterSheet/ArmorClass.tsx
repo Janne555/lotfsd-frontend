cc216354135eaabf34344cebf1f8276e7fc48ff8
Janne Lavila <janne.lavila@wheelq.com>
2019-12-01 16:42:53 +0200

    use selectors

--- src/Components/CharacterSheet/ArmorClass.tsx
+++ src/Components/CharacterSheet/ArmorClass.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { createUseStyles } from 'react-jss'
 import Cube from '../_shared/Cube'
 import Triangle from '../_shared/Triangle'
+import { useSelector } from '../../hooks'
+import { selectBaseArmorClass, selectRangedArmorClass, selectWithoutShieldArmorClass, selectSurprisedArmorClass } from '../../Redux/selectors'
 
 const useStyles = createUseStyles((theme: Theme) => ({
   armorClassRoot: {
@@ -26,16 +28,20 @@ const useStyles = createUseStyles((theme: Theme) => ({
 
 function ArmorClass() {
   const classes = useStyles()
+  const baseAC = useSelector(selectBaseArmorClass)
+  const rangedAC = useSelector(selectRangedArmorClass)
+  const withoutShieldAC = useSelector(selectWithoutShieldArmorClass)
+  const surprisedAC = useSelector(selectSurprisedArmorClass)
 
   return (
     <div className={classes.armorClassRoot}>
-      <Cube>1</Cube>
+      <Cube>{baseAC}</Cube>
       <h4>Mêlée</h4>
-      <Cube>1</Cube>
+      <Cube>{rangedAC}</Cube>
       <h4>Ranged</h4>
-      <Triangle>2</Triangle>
+      <Triangle>{withoutShieldAC}</Triangle>
       <h4>Without<br />Shield</h4>
-      <Triangle>2</Triangle>
+      <Triangle>{surprisedAC}</Triangle>
       <h4>Surprised</h4>
     </div>
   )
