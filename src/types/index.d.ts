09b597452faa5fd35d17471bd4284cee6a10ee11
Janne Lavila <janne.lavila@wheelq.com>
2019-12-01 16:00:42 +0200

    add new types

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -10,10 +10,52 @@ type Attributes = {
   wisdom: number
 }
 
+type AttributeModifiers = {
+  charisma: number
+  constitution: number
+  dexterity: number
+  intelligence: number
+  strength: number
+  wisdom: number
+}
+
 type SavingThrows = {
   paralyze: number
   poison: number
   breathWeapon: number
   magicalDevice: number
   magic: number
+}
+
+type Effect = AttributeModifierEffect | AttributeEffect
+
+type AttributeModifierEffect = {
+  type: "attributeModifierEffect"
+  target: keyof AttributeModifiers
+  value: number
+}
+
+type AttributeEffect = {
+  type: "attributeEffect"
+  target: keyof Attribute
+  value: number
+}
+
+type Equipment = Armor | Weapon
+
+type Armor = {
+  type: 'armor',
+  name: string,
+  baseArmorClass: number,
+  encumberance: number,
+  equipped: boolean
+}
+
+type Weapon = {
+  type: 'weappon',
+  name: string,
+  encumberance: number,
+  damage: string,
+  range: string,
+  equipped: boolean
 }
