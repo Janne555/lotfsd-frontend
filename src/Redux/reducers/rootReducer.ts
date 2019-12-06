f3990ab0f05c3cf14f13a14724e10141f87d9db5
Janne Lavila <janne.lavila@wheelq.com>
2019-12-06 19:15:02 +0200

    add reducer

--- src/Redux/reducers/rootReducer.ts
+++ src/Redux/reducers/rootReducer.ts
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
 import characterSheet from './characterSheet'
+import itemIndex from './itemIndex'
 
 export default combineReducers({
-  characterSheet
+  characterSheet,
+  itemIndex
 })
