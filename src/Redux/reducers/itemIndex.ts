76922dd6c91c18994210d21e06bffb81939b9f0e
Janne Lavila <janne.lavila@wheelq.com>
2019-12-06 19:14:45 +0200

    add reducer

---
+++ src/Redux/reducers/itemIndex.ts
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit'
+import { itemIndex } from '../../testData/initialState'
+
+const itemIndexSlice = createSlice({
+  name: 'itemIndex',
+  initialState: itemIndex,
+  reducers: {
+
+  }
+})
+
+export const {} = itemIndexSlice.actions
+
+export default itemIndexSlice.reducer
