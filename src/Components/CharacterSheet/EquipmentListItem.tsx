71e6ea510de179ada28ada0501d9ab841c433b97
Janne Lavila <janne.lavila@wheelq.com>
2019-12-07 15:47:22 +0200

    implement component

--- src/Components/CharacterSheet/EquipmentListItem.tsx
+++ src/Components/CharacterSheet/EquipmentListItem.tsx
@@ -3,28 +3,54 @@ import { createUseStyles } from 'react-jss'
 
 const useStyles = createUseStyles((theme: Theme) => ({
   name: {
-
+    gridColumnStart: 1
+  },
+  reserved: {
+    gridColumnStart: 1,
+    textAlign: 'center',
+    backgroundColor: theme.grey,
+    width: '100%',
+    height: '100%'
   },
   stack: {
-
+    gridColumnStart: 2,
+    justifySelf: 'end',
+  },
+  position: {
+    gridColumnStart: 3,
+    justifySelf: 'end'
   }
 }))
 
 type Props = {
   position: number
-  item: EquipmentListItem
+  label?: string
+  item?: EquipmentListItem
 }
 
-function ListItem({ item: { name, stackSize, amount, itemId }, position }: Props) {
+function EquipmentListItem({ item, position, label }: Props) {
   const classes = useStyles()
-  return (
-    <tr>
-      <td className={classes.name}>{name}</td>
-      <td className={classes.stack}>{stackSize > 1 ? `${amount}/${stackSize}` : ''}</td>
-      <td>{position + 1}</td>
-      <td>        </td>
-    </tr>
-  )
+
+
+  if (item) {
+    const { name, stackSize, amount, itemId } = item
+    return (
+      <>
+        <div className={classes.name}>{name}</div>
+        <div className={classes.stack}>{stackSize > 1 ? `${amount}/${stackSize}` : ''}</div>
+        <div className={classes.position}>{position + 1}</div>
+      </>
+    )
+  } else {
+    return (
+      <>
+        {label
+          ? <div className={classes.name}>{label}</div>
+          : <div className={classes.reserved}>reserved</div>
+        }
+      </>
+    )
+  }
 }
 
-export default ListItem
+export default EquipmentListItem
