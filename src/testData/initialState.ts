0779bd4a3b8fae83489a7eea14815e14f040dc27
Janne Lavila <janne.lavila@wheelq.com>
2019-12-10 16:25:28 +0200

    remove extras

--- src/testData/initialState.ts
+++ src/testData/initialState.ts
@@ -80,42 +80,6 @@ const inventory: ItemInstance[] = [
   {
     itemId: 'pavillion',
     instanceId: "7"
-  },
-  {
-    itemId: 'pavillion',
-    instanceId: "7"
-  },
-  {
-    itemId: 'pavillion',
-    instanceId: "7"
-  },
-  {
-    itemId: 'pavillion',
-    instanceId: "7"
-  },
-  {
-    itemId: 'pavillion',
-    instanceId: "7"
-  },
-  {
-    itemId: 'pavillion',
-    instanceId: "7"
-  },
-  {
-    itemId: 'pavillion',
-    instanceId: "7"
-  },
-  {
-    itemId: 'pavillion',
-    instanceId: "7"
-  },
-  {
-    itemId: 'pavillion',
-    instanceId: "7"
-  },
-  {
-    itemId: 'pavillion',
-    instanceId: "7"
   }
 ]
 
