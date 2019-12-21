48d17946de2a7a0c5f2e693db4ad0b7c4f43085e
Janne Lavila <janne.lavila@wheelq.com>
2019-12-21 18:46:48 +0200

    add reducer

--- src/Redux/reducers/characterSheet/savingThrows.ts
+++ src/Redux/reducers/characterSheet/savingThrows.ts
@@ -1,14 +1,16 @@
 import { savingThrows } from '../../../testData/initialState'
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
 const savingThrowsSlice = createSlice({
   name: 'savingThrows',
   initialState: savingThrows,
   reducers: {
-
+    setSavingThrow(state, action: PayloadAction<{ savingThrow: keyof SavingThrows, value: number }>) {
+      state[action.payload.savingThrow] = action.payload.value
+    }
   }
 })
 
-// export const { } = savingThrowsSlice.actions
+export const { setSavingThrow } = savingThrowsSlice.actions
 
 export default savingThrowsSlice.reducer
