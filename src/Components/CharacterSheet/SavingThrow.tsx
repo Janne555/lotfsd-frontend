b681fdb220f00a4e0fe7a5f32cf2c535c0e17f6d
Janne Lavila <janne.lavila@wheelq.com>
2019-12-21 18:47:19 +0200

    show modified unless editing

--- src/Components/CharacterSheet/SavingThrow.tsx
+++ src/Components/CharacterSheet/SavingThrow.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { createUseStyles } from 'react-jss'
 import { SAVING_THROW_DETAILS, SAVING_THROW_TITLES } from '../../constants'
 import RotatedCube from '../_shared/RotatedCube'
+import Input from '../_shared/Input'
+import { useDispatch } from '../../hooks'
+import { Validator } from '../../services'
+import { setSavingThrow } from '../../Redux/reducers/characterSheet/savingThrows'
 
 type Props = {
-  title: keyof SavingThrows,
-  value: number
+  name: keyof SavingThrows,
+  value: [number, number]
 }
 
 const useStyles = createUseStyles({
@@ -22,14 +26,40 @@ const useStyles = createUseStyles({
   }
 })
 
-export default function SavingThrow({ title, value }: Props) {
+const validator = new Validator().isLengthy.isNumber
+
+export default function SavingThrow({ name, value: [base, modified] }: Props) {
   const classes = useStyles()
+  const dispatch = useDispatch()
+  const [inFocus, setInFocus] = useState(false)
+
+  function handleChange(value: string) {
+    dispatch(setSavingThrow({ savingThrow: name, value: Number(value) }))
+  }
+
+  function handleFocus() {
+    setInFocus(true)
+  }
+
+  function handleBlur() {
+    setInFocus(false)
+  }
 
   return (
     <div className={classes.savingThrowRoot}>
-      <h3>{SAVING_THROW_TITLES[title]}</h3>
-      <RotatedCube>{value}</RotatedCube>
-      <span>{SAVING_THROW_DETAILS[title]}</span>
+      <h3>{SAVING_THROW_TITLES[name]}</h3>
+      <RotatedCube>
+        <Input
+          isValid={validator.validate}
+          onChange={handleChange}
+          value={`${inFocus ? base : modified}`}
+          inputProps={{
+            onFocus: handleFocus,
+            onBlur: handleBlur
+          }}
+        />
+      </RotatedCube>
+      <span>{SAVING_THROW_DETAILS[name]}</span>
     </div>
   )
 }
