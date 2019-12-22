f24c301e1f86cdfc34df95b896048f2acc352943
Janne Lavila <janne.lavila@wheelq.com>
2019-12-22 18:52:23 +0200

    add constants

---
+++ src/constants/characterClasses.ts
@@ -0,0 +1,14 @@
+const characterClasses = {
+  fighter: {
+    savingThrows: {
+      breathWeapon: 10,
+      magic: 11,
+      magicalDevice: 12,
+      paralyze: 13,
+      poison: 14
+    },
+    levels: [
+      0, 2000, 4000, 8000, 16000, 32000, 64000
+    ]
+  }
+}
