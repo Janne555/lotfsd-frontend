dca4ac6ccbceea5b7b6feaff7123e6d10820f21f
Janne Lavila <janne.lavila@wheelq.com>
2019-12-26 11:04:55 +0200

    wip item index

--- src/Components/ItemIndex/ItemIndex.tsx
+++ src/Components/ItemIndex/ItemIndex.tsx
@@ -1,20 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { createUseStyles } from 'react-jss'
 import { useSelector } from '../../hooks'
 import { selectItemIndex } from '../../Redux/selectors'
-import { Link, Route, useRouteMatch, useHistory } from 'react-router-dom'
+import { Route, useRouteMatch, useHistory } from 'react-router-dom'
 import ItemDetails from '../_shared/ItemDetails'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
+import TextField from '@material-ui/core/TextField'
 
 const useStyles = createUseStyles((theme: Theme) => ({
   itemIndex: {
     display: 'grid',
     gridTemplateColumns: '33% auto',
-    '& a': {
-      textDecoration: 'none',
-    }
+    padding: theme.padding
   },
   list: {
     listStyleType: 'none'
@@ -22,6 +21,9 @@ const useStyles = createUseStyles((theme: Theme) => ({
   listItem: {
     minHeight: '2rem',
     outline: '1px solid grey'
+  },
+  searchBox: {
+    width: '100%'
   }
 }))
 
@@ -31,29 +33,37 @@ type Props = {
 
 function ItemIndex({ }: Props) {
   const classes = useStyles()
+  const [searchTerm, setSearchTerm] = useState("")
   const items = useSelector(selectItemIndex)
+    .filter(item => item.name.toLowerCase().includes(searchTerm.toLowerCase()))
   const match = useRouteMatch<{ item: string }>("/itemindex/:item")
   const history = useHistory()
   const selected = items.find(item => item.name === match?.params.item)
 
-  function handleItemClick(e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
-    e.persist()
-    e.preventDefault()
-    console.log(e)
-    history.push(``)
+  function handleItemClick(item: string) {
+    history.push(`/itemindex/${item}`)
   }
 
   return (
     <div className={classes.itemIndex}>
-      <List>
-        {
-          items.map(item => (
-            <ListItem component href={`/itemindex/${item.name}`} key={item.id} id={item.name} onClick={handleItemClick} >
-              <ListItemText primary={item.name} />
-            </ListItem>
-          ))
-        }
-      </List>
+      <div>
+        <TextField className={classes.searchBox} label="Search" onChange={e => setSearchTerm(e.target.value)} />
+        <List>
+          {
+            items.map((item: Item) => (
+              <ListItem
+                button
+                key={item.id}
+                id={item.name}
+                onClick={() => handleItemClick(item.name)}
+                selected={item.name === selected?.name}
+              >
+                <ListItemText primary={item.name} />
+              </ListItem>
+            ))
+          }
+        </List>
+      </div>
       <Route path="/itemindex/:item">
         {selected &&
           <ItemDetails item={selected} />
