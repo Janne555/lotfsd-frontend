d5fb75020b5be38ec30efc7b4a72771ffa29122f
Janne Lavila <janne.lavila@wheelq.com>
2019-12-28 18:20:09 +0200

    add login action

--- src/Redux/specialActions.ts
+++ src/Redux/specialActions.ts
@@ -2,6 +2,9 @@ import { createAction } from '@reduxjs/toolkit'
 
 const createCharacter = createAction<NewCharacterPayload>('createCharacter')
 
+const login = createAction<LoginPayload>('login')
+
 export {
-  createCharacter
+  createCharacter,
+  login
 }
