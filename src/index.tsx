6a3a303606464ff446801167ca0609eb05f195e1
Janne Lavila <janne.lavila@wheelq.com>
2020-01-09 17:39:55 +0200

    add environment provider

--- src/index.tsx
+++ src/index.tsx
@@ -7,13 +7,18 @@ import { Provider } from 'react-redux'
 import { ThemeProvider } from 'react-jss'
 import { theme } from './styles/theme'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { RelayEnvironmentProvider } from 'relay-hooks'
+import { environment } from './services'
+
 import './index.css'
 
 ReactDOM.render(
   <Router>
     <ThemeProvider theme={theme}>
       <Provider store={store}>
-        <App />
+        <RelayEnvironmentProvider environment={environment}>
+          <App />
+        </RelayEnvironmentProvider>
       </Provider>
     </ThemeProvider>
   </Router>,
