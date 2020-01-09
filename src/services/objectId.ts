b7b9bea40aacbe0e46605a0bf4bc952b6317daff
Janne Lavila <janne.lavila@wheelq.com>
2020-01-09 17:40:26 +0200

    add id gen

---
+++ src/services/objectId.ts
@@ -0,0 +1,8 @@
+function mongoObjectId() { //https://gist.github.com/solenoid/1372386
+  var timestamp = (new Date().getTime() / 1000 | 0).toString(16);
+  return timestamp + 'xxxxxxxxxxxxxxxx'.replace(/[x]/g, function () {
+    return (Math.random() * 16 | 0).toString(16);
+  }).toLowerCase();
+}
+
+export { mongoObjectId }
