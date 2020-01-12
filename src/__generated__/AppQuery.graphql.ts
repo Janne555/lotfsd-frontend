fa4cb595790f9cea9c9f5f4ee8ed56e2023063aa
Janne Lavila <janne.lavila@wheelq.com>
2020-01-12 16:36:03 +0200

    generated

---
+++ src/__generated__/AppQuery.graphql.ts
@@ -0,0 +1,91 @@
+/* tslint:disable */
+/* @relayHash 20cb22cdb61cf72f935319b7835cb422 */
+
+import { ConcreteRequest } from "relay-runtime";
+export type AppQueryVariables = {};
+export type AppQueryResponse = {
+    readonly characterSheets: ReadonlyArray<{
+        readonly name: string;
+        readonly class: string;
+        readonly id: string;
+    } | null> | null;
+};
+export type AppQuery = {
+    readonly response: AppQueryResponse;
+    readonly variables: AppQueryVariables;
+};
+
+
+
+/*
+query AppQuery {
+  characterSheets {
+    name
+    class
+    id
+  }
+}
+*/
+
+const node: ConcreteRequest = (function(){
+var v0 = [
+  {
+    "kind": "LinkedField",
+    "alias": null,
+    "name": "characterSheets",
+    "storageKey": null,
+    "args": null,
+    "concreteType": "CharacterSheet",
+    "plural": true,
+    "selections": [
+      {
+        "kind": "ScalarField",
+        "alias": null,
+        "name": "name",
+        "args": null,
+        "storageKey": null
+      },
+      {
+        "kind": "ScalarField",
+        "alias": null,
+        "name": "class",
+        "args": null,
+        "storageKey": null
+      },
+      {
+        "kind": "ScalarField",
+        "alias": null,
+        "name": "id",
+        "args": null,
+        "storageKey": null
+      }
+    ]
+  }
+];
+return {
+  "kind": "Request",
+  "fragment": {
+    "kind": "Fragment",
+    "name": "AppQuery",
+    "type": "Query",
+    "metadata": null,
+    "argumentDefinitions": [],
+    "selections": (v0/*: any*/)
+  },
+  "operation": {
+    "kind": "Operation",
+    "name": "AppQuery",
+    "argumentDefinitions": [],
+    "selections": (v0/*: any*/)
+  },
+  "params": {
+    "operationKind": "query",
+    "name": "AppQuery",
+    "id": null,
+    "text": "query AppQuery {\n  characterSheets {\n    name\n    class\n    id\n  }\n}\n",
+    "metadata": {}
+  }
+};
+})();
+(node as any).hash = '8df68ba5595caeaa4c2f981898d002d1';
+export default node;
