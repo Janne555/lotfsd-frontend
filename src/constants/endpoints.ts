9b9de8f3dc029af7bf2deca21f5a1e98fcdc79f1
Janne Lavila <Janne555@users.noreply.github.com>
2020-01-22 21:12:16 +0200

    update prod endpoint

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -1,4 +1,4 @@
-const APIROOT = process.env.NODE_ENV === 'production' ? "dunno lol" : "http://localhost:5000/"
+const APIROOT = process.env.NODE_ENV === 'production' ? "https://lotfsdapi.azurewebsites.net/" : "http://localhost:5000/"
 
 const ENDPOINTS = {
   LOGIN: { url: 'user/login', type: "" }
