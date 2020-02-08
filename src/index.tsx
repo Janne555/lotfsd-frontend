3f395ed806d0c69800615494eef568b1a738eec4
Janne Lavila <Janne555@users.noreply.github.com>
2020-02-08 11:14:20 +0200

    extract component

--- src/index.tsx
+++ src/index.tsx
@@ -1,42 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Main from './Main'
 import * as serviceWorker from './serviceWorker'
 import { ThemeProvider } from 'react-jss'
 import { theme } from './styles/theme'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { createApolloClient } from './services'
-import { useLoginStatus, LoginProvider, useToken } from './hooks'
-import Login from './Components/Interface/Login'
-import Register from './Components/Interface/Register'
-import { ApolloProvider } from '@apollo/react-hooks'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { LoginProvider } from './hooks'
 import 'normalize.css'
 import './index.css'
-
-function App() {
-  const loginStatus = useLoginStatus()
-  const token = useToken()
-  const client = React.useMemo(() => token ? createApolloClient(token) : undefined, [token])
-
-  if (loginStatus === "logged-in" && client) {
-    return (
-      <ApolloProvider client={client}>
-        <Main />
-      </ApolloProvider>
-    )
-  } else {
-    return (
-      <Switch>
-        <Route path="/register">
-          <Register />
-        </Route>
-        <Route>
-          <Login />
-        </Route>
-      </Switch>
-    )
-  }
-}
+import App from './App'
 
 ReactDOM.render(
   <Router>
